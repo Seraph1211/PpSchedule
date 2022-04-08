c0c5498951fcc12586bee4cc3b7b750fe09d2fa0
Peng <peng@gmail.com>
2022-04-09 02:22:58 +0800

    成功调用com.jeek.calendar相关控件

--- app/src/main/java/com/seraph/ppschedule/activity/MainActivity.java
+++ app/src/main/java/com/seraph/ppschedule/activity/MainActivity.java
@@ -1,58 +1,39 @@
 package com.seraph.ppschedule.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Gallery;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.seraph.ppschedule.R;
-import com.seraph.ppschedule.bean.Schedule;
-import com.seraph.ppschedule.dao.ScheduleDao;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Iterator;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    ScheduleDao dao = new ScheduleDao();
-    List<Schedule> scheduleList;
+
+    private DrawerLayout mDrawer;
+    private ImageButton btnMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mDrawer = findViewById(R.id.dlMain);
+        btnMenu = findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawer.openDrawer(Gravity.START);
+            }
+        });
+
     }
 
-    public void add(View view) {
-        dao.addSchedule("TITLE", "DESC", Calendar.getInstance());
-    }
-
-    public void show(View view) {
-        scheduleList = dao.findAllSchedule();
-
-        if(scheduleList.size() == 0) {
-            Toast.makeText(MainActivity.this, "show(): ScheduleList is null !", Toast.LENGTH_SHORT).show();
-        }
-
-        Iterator<Schedule> iterator = scheduleList.iterator();
-        while(iterator.hasNext()) {
-            Schedule schedule = iterator.next();
-            Log.d(TAG, "[id=" + schedule.getId() + ", title=" + schedule.getTitle()
-                    + ", time=" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(schedule.getDate().getTime()) + "]");
-        }
-    }
-
-    public void delete(View view) {
-        if(scheduleList == null || scheduleList.size() <= 0) {
-            Toast.makeText(MainActivity.this, "delete(): ScheduleList is null !", Toast.LENGTH_SHORT).show();
-            return ;
-        }
-
-        dao.deleteSchedule(scheduleList.size());
-    }
 }
